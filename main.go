f3831fc331eb208863e73831dbc6f86c66205be4
rayan <rayanzyad99@gmail.com>
2024-02-24 00:05:12 +0000

    Start

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
+// like a box where you can store different kinds of things together,
+// like a person's name and age. It helps you keep related information organized in one place
 type Movie struct {
 	ID       string    `json:"id"`
 	Isbn     string    `json:"isbn"`
 	Title    string    `json:"title"`
-	Director *Director `json:"director"`
+	Director *Director `json:"director"` // The * says it might be null
 }
 
 type Director struct {
