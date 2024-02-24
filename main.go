f095195aee7a4a44fa9efcc3943c08d2ab56d4a4
rayan <rayanzyad99@gmail.com>
2024-02-24 00:11:18 +0000

    Start

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import "github.com/gorilla/mux"
+
 // like a box where you can store different kinds of things together,
 // like a person's name and age. It helps you keep related information organized in one place
 type Movie struct {
@@ -12,4 +14,12 @@ type Movie struct {
 type Director struct {
 	Firstname string `json:"firstname"`
 	Lastname  string `json:"lastname"`
+}
+
+var movies []Movie
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}")
 }
