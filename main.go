00626e502ae3c5ac7dabd3c05b6883b66c7272fd
rayan <rayanzyad99@gmail.com>
2024-02-24 00:17:11 +0000

    Start

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-import "github.com/gorilla/mux"
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
 
 // like a box where you can store different kinds of things together,
 // like a person's name and age. It helps you keep related information organized in one place
@@ -21,5 +26,11 @@ var movies []Movie
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/movies", getMovies).Methods("GET")
-	r.HandleFunc("/movies/{id}")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Println("Starting server at port 8000 \n")
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
